2a3a1d3727e25c206d224d0f548c690e17208f21
WriestTavo <sotosoto_96@hotmail.com>
2018-09-03 11:58:58 -0700

    se agrego practica del ciclo ford

---
+++ js/12_Ford/ford.js
@@ -0,0 +1,25 @@
+/*
+*   Ciclo ford
+*/
+
+
+/*
+ford (let i = ;condiction; incremento){
+console.log(`msg`);
+}
+*/
+
+for (let i = 0; i < 10 ;i++){
+    console.log(`El valor de i es ${i}`);
+}
+
+
+
+
+
+
+
+
+
+
+// Gustavo A. Soto ( WriestTavo ) 
